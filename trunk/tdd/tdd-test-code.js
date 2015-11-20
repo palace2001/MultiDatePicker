ac532bb6cb7cd2cba6e51f9c3ecbf3d147e9d795
palace2001 <KimKeunPeel@gimgeunpilui-MacBook-Pro.local>
2015-11-20 15:33:57 +0900

    Test2.

--- trunk/tdd/tdd-test-code.js
+++ trunk/tdd/tdd-test-code.js
@@ -5,15 +5,27 @@ module("Test 1")
 QUnit.test( "Today Testing", function( assert ) {
 
 	
-    var result1 = set_CurrentDate();
-	var result2 = set_CurrentMonth();
-	var result3 = set_CurrentYear();
-	var result4 = set_CurrentDay(); 
+    var resultDate = set_CurrentDate();
+	var resultMonth = set_CurrentMonth();
+	var resultYear = set_CurrentYear();
+	var resultDay = set_CurrentDay(); 
+	var resultToday = typeof(set_Today());
 	
-	equal(result1,20,"Testing Passed!");
-	equal(result2,11,"Testing Passed!");
-	equal(result3,2015,"Testing Passed!");
-	equal(result4, 5,"Testing Passed!");	  
+	equal(resultDate, 20,"Testing Passed!");
+	equal(resultMonth, 11,"Testing Passed!");
+	equal(resultYear, 2015,"Testing Passed!");
+	equal(resultDay, 5,"Testing Passed!");
+	equal(resultToday,'string',"Testing Passed!");	  
+});
+
+module("Test 2")
+QUnit.test( "Day Of This Month Testing", function( assert ) {
+	  var resultFirstDay = get_FirstDay();
+	  var resultLastDay = get_LastDay(2015,11);
+
+	  equal(resultFirstDay, 0,"Testing Passed!");
+	  equal(resultLastDay, 1,"Testing Passed!");
+
 });
 
 
