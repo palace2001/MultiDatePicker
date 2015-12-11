550ed0875de340724b5cf759d789ae7a60fbc46d
이가람 <popotan90@gmail.com>
2015-12-11 15:01:21 +0900

    이걸 놓쳤다니..

--- trunk/core/core.js
+++ trunk/core/core.js
@@ -40,16 +40,16 @@ function bindingClickEventForHeaderButton(){
  * Function : set_Today
  *   - 오늘의 요일를 구함
  */
-function set_Today() {
-	var index = today.getDay();
-	return convertDateToText[index];
-}
+ function set_Today() {
+ 	var index = today.getDay();
+ 	return convertDateToText[index];
+ }
 
 /**
  * Function : get_FirstDay
  *   - 해당하는 달 1일의 요일을 구함
  *   - ex) 만약 월요일이라면 월요일(1)을 반환함
- */
+*/
 function get_FirstDay(){
 	var d = new Date(year,month-1,1);
 	return d.getDay();
@@ -59,30 +59,30 @@ function get_FirstDay(){
  * Function : get_LastDay
  *   - get_day_max 함수를 이용하여 마지막 일의 요일을 구함
  */
-function get_LastDay(){
-	var d = new Date(year,month-1,get_day_max(year,month-1));
-	return d.getDay();
-}
+ function get_LastDay(){
+ 	var d = new Date(year,month-1,get_day_max(year,month-1));
+ 	return d.getDay();
+ }
 
 /**
  * Function : get_day_max
  *   - parameter로 받아오는 년(year),월(month)의 마지막 일을 구함
  */
-function get_day_max(year,month){
-	var i = 29, cday;
-	while(i < 32){
-		cday = new Date(year,month,i);
-		if (cday.getFullYear()!=year || cday.getMonth()!=month) break;
-		i++;
-	}
-	return i-1;
-}
+ function get_day_max(year,month){
+ 	var i = 29, cday;
+ 	while(i < 32){
+ 		cday = new Date(year,month,i);
+ 		if (cday.getFullYear()!=year || cday.getMonth()!=month) break;
+ 		i++;
+ 	}
+ 	return i-1;
+ }
 
 /**
  * Function : draw_prevBlank
  *   - for문을 이용하여 1일 시작 전까지의 공백을 그림
  *   - ex) 만약 1일이 수요일이라면 일요일, 월요일, 화요일 공백 칸을 생성
- */
+*/
 function draw_prevBlank(){
 	var str = '<ul class="row">';
 	for (var i = 0; i < get_FirstDay(); i++) {
@@ -97,231 +97,234 @@ function draw_prevBlank(){
  *   - ex) 만약 마지막 날이 목요일이라면 금요일, 토요일 칸을 생성해야함 => 해당 월 마지막 공백 칸의 수를 리턴
  *   - (+) 마지막날이 토요일이면 추가로 생성할 필요가 없으므로 실행하지 않음
  */
-function get_LastBlank(){
-	if(get_LastDay()!=0){
-		return 6-get_LastDay();
-	}
-	return 6;
-}
+ function get_LastBlank(){
+ 	if(get_LastDay()!=0){
+ 		return 6-get_LastDay();
+ 	}
+ 	return 6;
+ }
 
 /**
  * Function : draw_LastBlank
  *   - 해당하는 월의 마지막 공백을 그림
  */
-function draw_LastBlank(){
-	var str = '';
-	for (var i = 0; i < get_LastBlank(); i++) {
-		str += '<li class="date"> . </li>';
-	}
-	str += '</ul>';
-	return str;
-}
+ function draw_LastBlank(){
+ 	var str = '';
+ 	for (var i = 0; i < get_LastBlank(); i++) {
+ 		str += '<li class="date"> . </li>';
+ 	}
+ 	str += '</ul>';
+ 	return str;
+ }
 
 /**
  * Function : prevButton
  *   - 1년전, 1달전으로 돌아감
  */
-function prevButton(){
-	switch(mode){
-		case 'ymd':
-		if(month != 1)
-		{
-			month --;
-		}else{
-			year --;
-			month = 12;
-		}
-		ymdDraw();
-		break;
-		default:
-		year = year - 7;
-		yearDraw();
-		break;
-	}
-}
+ function prevButton(){
+ 	switch(mode){
+ 		case 'ymd':
+ 		if(month != 1)
+ 		{
+ 			month --;
+ 		}else{
+ 			year --;
+ 			month = 12;
+ 		}
+ 		ymdDraw();
+ 		break;
+ 		default:
+ 		year = year - 7;
+ 		yearDraw();
+ 		break;
+ 	}
+ }
 
 /**
  * Function : todayButton
  *   - 다른날에서 오늘로 돌아감
  */
-function todayButton(){
-	year = today.getFullYear();
-	month = today.getMonth() + 1;
-	day = today.getDate();
-	switch(mode){
-		case 'ymd':
-		ymdDraw();
-		break;
-		default:
-		yearDraw();
-		break;
-	}
-}
+ function todayButton(){
+ 	year = today.getFullYear();
+ 	month = today.getMonth() + 1;
+ 	day = today.getDate();
+ 	switch(mode){
+ 		case 'ymd':
+ 		ymdDraw();
+ 		break;
+ 		default:
+ 		yearDraw();
+ 		break;
+ 	}
+ }
 
 /**
  * Function : nextButton
  *   - 
  */
-function nextButton(){
-	switch(mode){
-		case 'ymd':
-		if(month != 12)
-		{
-			month ++;
-		}else{
-			year ++;
-			month = 1;
-		}
-		ymdDraw();
-		break;
-		default:
-		year = year + 7;
-		yearDraw();
-		break;
-	}
-}
+ function nextButton(){
+ 	switch(mode){
+ 		case 'ymd':
+ 		if(month != 12)
+ 		{
+ 			month ++;
+ 		}else{
+ 			year ++;
+ 			month = 1;
+ 		}
+ 		ymdDraw();
+ 		break;
+ 		default:
+ 		year = year + 7;
+ 		yearDraw();
+ 		break;
+ 	}
+ }
 
 /**
  * Function : ySelectMode
  *   - 
  */
-function ySelectMode(){
-	mode = 'y';
-	headerDraw("y");
-	yearDraw();
-	bindingClickEventForHeaderButton();
-}
+ function ySelectMode(){
+ 	mode = 'y';
+ 	headerDraw("y");
+ 	yearDraw();
+ 	bindingClickEventForHeaderButton();
+ }
 
 /**
  * Function : ymSelectMode
  *   - 
  */
-function ymSelectMode(){
-	mode = 'ym';
-	headerDraw("ym");
-	ymDraw();
-	yearDraw();
-	bindingClickEventForHeaderButton();
-}
+ function ymSelectMode(){
+ 	mode = 'ym';
+ 	headerDraw("ym");
+ 	ymDraw();
+ 	yearDraw();
+ 	bindingClickEventForHeaderButton();
+ }
 
 /**
  * Function : ymdSelectMode
  *   - 
  */
-function ymdSelectMode(){
-	mode = 'ymd';
-	headerDraw("ymd");
-	ymdDraw();
-	bindingClickEventForHeaderButton();
-}
+ function ymdSelectMode(){
+ 	mode = 'ymd';
+ 	headerDraw("ymd");
+ 	ymdDraw();
+ 	bindingClickEventForHeaderButton();
+ }
 
-function addClassForSelected(pickeddate)
-{
-	$("li[datevalue='"+pickeddate+"']").addClass("selected");
-}
+ function addClassForSelected(pickeddate)
+ {
+ 	$("li[datevalue='"+pickeddate+"']").addClass("selected");
+ }
 
-function deletePickedDate(pickeddate)
-{
-	$("li[datevalue='"+pickeddate+"']").removeClass("selected");
+ function deletePickedDate(pickeddate)
+ {
+ 	$("li[datevalue='"+pickeddate+"']").removeClass("selected");
 
-	for (var i = 0; i < selectedDate.length; i++) {
-		if(selectedDate[i] == pickeddate){
-			selectedDate.splice(i,1);
-			i--;
-		}
-	}
-}
+ 	for (var i = 0; i < selectedDate.length; i++) {
+ 		if(selectedDate[i] == pickeddate){
+ 			selectedDate.splice(i,1);
+ 			i--;
+ 		}
+ 	}
+ }
 
 /**
  * Function : pushSelectedDate
  *   - 
  */
-function pushSelectedDate(pickeddate){
-	if(checkDuplicationData(pickeddate)){
-		deletePickedDate(pickeddate);
-	}else{
-		selectedDate.push(pickeddate);
-		addClassForSelected(pickeddate);
-		console.log(selectedDate);
-	}
-}
+ function pushSelectedDate(pickeddate){
+ 	if(checkDuplicationData(pickeddate)){
+ 		deletePickedDate(pickeddate);
+ 	}else{
+ 		selectedDate.push(pickeddate);
+ 		addClassForSelected(pickeddate);
+ 		console.log(selectedDate);
+ 	}
+ }
 
 /**
  * Function : pushSelectedMonth
  *   - 
  */
-function pushSelectedMonth(pickedMonth){
-	if (checkDuplicationData(pickedMonth)) {
-		deletePickedDate(pickeddate);
-	}else{
-		selectedDate.push(pickedMonth);
-		console.log(selectedDate);
-	}
-}
+ function pushSelectedMonth(pickedMonth){
+ 	if (checkDuplicationData(pickedMonth)) {
+ 		deletePickedDate(pickedMonth);
+ 	}else{
+ 		selectedDate.push(pickedMonth);
+ 		console.log(selectedDate);
+ 	}
+ }
 
 /**
  * Function : pushSelectedYear
  *   - 
  */
-function pushSelectedYear(pickedYear){
-	if (checkDuplicationData(pickedYear)) {
-		deletePickedDate(pickeddate);
-	}else{
-		selectedDate.push(pickedYear);
-		console.log(selectedDate);
-	}
-}
+ function pushSelectedYear(pickedYear){
+ 	if (checkDuplicationData(pickedYear)) {
+ 		deletePickedDate(pickedYear);
+ 	}else{
+ 		selectedDate.push(pickedYear);
+ 		addClassForSelected(pickedYear);
+ 		console.log(selectedDate);
+ 	}
+ }
 
 /**
  * Function : checkDuplicationData
  *   - 
  */
-function checkDuplicationData(pickeddate){
-	for (var i = 0; i < selectedDate.length; i++) {
-		if(selectedDate[i] == pickeddate){
-			return true;
-		}
-	}
-	return false;
-}
+ function checkDuplicationData(pickeddate){
+ 	for (var i = 0; i < selectedDate.length; i++) {
+ 		if(selectedDate[i] == pickeddate){
+ 			return true;
+ 		}
+ 	}
+ 	return false;
+ }
 
 /**
  * Function : bindingDateClickEvent
  *   - 
  */
-function bindingDateClickEvent(){
-	$(".date").bind("click", function(e){	
-		pushSelectedDate($(this).attr("dateValue"));
-	});
-}
+ function bindingDateClickEvent(){
+ 	$(".date").bind("click", function(e){
+ 		if($(this).attr("dateValue") !== undefined){
+ 			pushSelectedDate($(this).attr("dateValue"));
+ 		}
+ 	});
+ }
 
 /**
  * Function : bindingMonthClickEvent
  *   - 
  */
-function bindingMonthClickEvent(){
-	$("#monthSelect li").bind("click", function(e){
-		if(clickedYear == null){
-			alert("연도를 선택해 주세요!");
-		}else{
-			pushSelectedMonth(clickedYear + '-' + $(this).attr("dateValue"));
-		}
-	});
-}
+ function bindingMonthClickEvent(){
+ 	$("#monthSelect li").bind("click", function(e){
+ 		if(clickedYear == null){
+ 			alert("연도를 선택해 주세요!");
+ 		}else{
+ 			pushSelectedMonth(clickedYear + '-' + $(this).attr("dateValue"));
+ 		}
+ 	});
+ }
 
 /**
  * Function : bindingYearClickEvent
  *   - 
  */
-function bindingYearClickEvent(){
-	$("#yearSelect li").bind("click", function(e){
-		clickedYear = $(this).attr("dateValue");
-		console.log(clickedYear);
-		if(mode == 'y'){
-			pushSelectedYear(clickedYear);
-		}
-	});
-}
+ function bindingYearClickEvent(){
+ 	$("#yearSelect li").bind("click", function(e){
+ 		clickedYear = $(this).attr("dateValue");
+ 		console.log(clickedYear);
+ 		if(mode == 'y'){
+ 			pushSelectedYear(clickedYear);
+ 		}
+ 	});
+ }
 
 //drawing functions area
 
@@ -329,85 +332,85 @@ function bindingYearClickEvent(){
  * Function : headerDraw
  *   - 
  */
-function headerDraw(mode){
-	switch(mode){
-		case 'ymd':
-		var str = '<div id="header"><span id="left"><span id="modeSelectButtonGroup"><button id="y-select">년</button><button id="ym-select">년-월</button><button id="ymd-select">년-월-일</button></span></span><span id="center"><h1 id="calendarTitle"><span id="year"></span>년 <span id="month"></span>월</h1></span><span id="right"><span id="moveButtonGroup"><button id="prev">이전</button><button id="today">오늘</button><button id="next">다음</button></span></span></div>';
-		break;
-		default:
-		var str = '<div id="header"><span id="left"><span id="modeSelectButtonGroup"><button id="y-select">년</button><button id="ym-select">년-월</button><button id="ymd-select">년-월-일</button></span></span><span id="center"><ul id="yearSelect"></ul></span><span id="right"><span id="moveButtonGroup"><button id="prev">이전</button><button id="today">오늘</button><button id="next">다음</button></span></span>';
-		break;
-	}
-	str += '<div id="content"></div>';
-	$(".multiDatePicker").empty().append(str);
-}
+ function headerDraw(mode){
+ 	switch(mode){
+ 		case 'ymd':
+ 		var str = '<div id="header"><span id="left"><span id="modeSelectButtonGroup"><button id="y-select">년</button><button id="ym-select">년-월</button><button id="ymd-select">년-월-일</button></span></span><span id="center"><h1 id="calendarTitle"><span id="year"></span>년 <span id="month"></span>월</h1></span><span id="right"><span id="moveButtonGroup"><button id="prev">이전</button><button id="today">오늘</button><button id="next">다음</button></span></span></div>';
+ 		break;
+ 		default:
+ 		var str = '<div id="header"><span id="left"><span id="modeSelectButtonGroup"><button id="y-select">년</button><button id="ym-select">년-월</button><button id="ymd-select">년-월-일</button></span></span><span id="center"><ul id="yearSelect"></ul></span><span id="right"><span id="moveButtonGroup"><button id="prev">이전</button><button id="today">오늘</button><button id="next">다음</button></span></span>';
+ 		break;
+ 	}
+ 	str += '<div id="content"></div>';
+ 	$(".multiDatePicker").empty().append(str);
+ }
 
 /**
  * Function : ymdDraw
  *   - 
  */
-function ymdDraw() {
-	var str = '<ul class="row">';
-	for (var i = 0; i < 7; i++) {
-		str += '<li class="weekText">' + convertDateToText[i] + '</li>';
-	}
-	str += '</ul>';
+ function ymdDraw() {
+ 	var str = '<ul class="row">';
+ 	for (var i = 0; i < 7; i++) {
+ 		str += '<li class="weekText">' + convertDateToText[i] + '</li>';
+ 	}
+ 	str += '</ul>';
 
-	var firstDay = get_FirstDay() + 1;
+ 	var firstDay = get_FirstDay() + 1;
 
-	str += draw_prevBlank();
+ 	str += draw_prevBlank();
 
-	var dateNum = 1;
-	for(firstDay; dateNum <= get_day_max(year,month-1) ; firstDay++){
-		str += '<li class="date" dateValue="' + year + '-' + month + '-' + dateNum + '">' + dateNum + '</li>';
-		if(firstDay%7==0){
-			str += '</ul><ul class="row">';
-		}
-		dateNum++;
-	}
+ 	var dateNum = 1;
+ 	for(firstDay; dateNum <= get_day_max(year,month-1) ; firstDay++){
+ 		str += '<li class="date" dateValue="' + year + '-' + month + '-' + dateNum + '">' + dateNum + '</li>';
+ 		if(firstDay%7==0){
+ 			str += '</ul><ul class="row">';
+ 		}
+ 		dateNum++;
+ 	}
 
-	str += draw_LastBlank();
+ 	str += draw_LastBlank();
 
-	$("#year").empty().append(year);
-	$("#month").empty().append(month);
-	$("#content").empty().append(str);
-	bindingDateClickEvent();
-}
+ 	$("#year").empty().append(year);
+ 	$("#month").empty().append(month);
+ 	$("#content").empty().append(str);
+ 	bindingDateClickEvent();
+ }
 
 /**
  * Function : ymDraw
  *   - 
  */
-function ymDraw(){
-	var str = '<ul id="monthSelect">';
-	for (var i = 1; i < 13; i++) {
-		str += '<li dateValue="' + i + '">' + i + '</li>';
-	};
-	$("#content").append(str);
-	bindingMonthClickEvent();
-}
+ function ymDraw(){
+ 	var str = '<ul id="monthSelect">';
+ 	for (var i = 1; i < 13; i++) {
+ 		str += '<li class="date" dateValue="' + i + '">' + i + '</li>';
+ 	};
+ 	$("#content").append(str);
+ 	bindingMonthClickEvent();
+ }
 
 /**
  * Function : yearDraw
  *   - 
  */
-function yearDraw(){
-	var str = '';
-	for (var i = 0; i < 7; i++) {
-		str += '<li dateValue="' + (year + i - 3) + '">' + (year + i - 3) + '</li>';
-	}
-	$("#yearSelect").empty().append(str);
-	bindingYearClickEvent();
-}
+ function yearDraw(){
+ 	var str = '';
+ 	for (var i = 0; i < 7; i++) {
+ 		str += '<li class="date" dateValue="' + (year + i - 3) + '">' + (year + i - 3) + '</li>';
+ 	}
+ 	$("#yearSelect").empty().append(str);
+ 	bindingYearClickEvent();
+ }
 
 /**
  * Function : setRowHeight
  *   - 
  */
-function setRowHeight(){
-	$(".date").each(function(){
-		var row = $(this).parent('.row');
-		var rowheight = $(row).height();
-		$(this).css("height", rowheight);
-	});
-}
+ function setRowHeight(){
+ 	$(".date").each(function(){
+ 		var row = $(this).parent('.row');
+ 		var rowheight = $(row).height();
+ 		$(this).css("height", rowheight);
+ 	});
+ }
